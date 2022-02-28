f09e12873a9c3850be66cb2537044235f9e6f74d
Jaedson Silva <contacanal798@gmail.com>
2022-02-28 19:10:18 -0300

    adicionando "variavies"

---
+++ variaveis/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    // números
+    int myInt = 10;
+    int myNegativeInt = -10;
+    float myFloat = 5.7;
+
+    // números sem sinais (apenas números positivos)
+    unsigned int myPositiveInt = 10;
+    
+    // char
+    char myChar = 'a'; // apenas um caractere (aspas simples)
+    char myCharArray[10] = "012345678"; // máximo é 9 (aspas duplas)
+
+    // string
+    string myString = "I'm a developer, bro.";
+
+    // bool
+    bool isFalse = true;
+    bool isTrue = false;
+
+    return 0;
+}
