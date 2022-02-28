f53bdcfc6157325e9a93a088e199d6836bd3dd43
Jaedson Silva <contacanal798@gmail.com>
2022-02-28 19:27:33 -0300

    adicionando "if-else"

---
+++ if-else/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int myNumber = 10;
+    int yourNumber = 15;
+
+    if(myNumber == yourNumber) {
+        cout << "Os números são iguais!" << endl;
+    } else {
+        cout << "Os números são diferentes!" << endl;
+    }
+
+    return 0;
+}
