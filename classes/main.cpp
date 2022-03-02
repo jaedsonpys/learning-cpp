7c418b088ba81e1c1f3f437038af4a55a391d8fa
Jaedson Silva <contacanal798@gmail.com>
2022-03-02 19:21:13 -0300

    adicionando "classes"

---
+++ classes/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Human {
+    public: // acessável fora da classe
+        string name;
+        int age;
+
+        Human(string name, int age) {
+            this->name = name;
+            this->age = age;
+        };
+
+        void printID() {
+            cout << "Nome: " << this->name << endl;
+            cout << "Idade: " << this->age << endl;
+        };
+
+        void changeAge(int newAge) {
+            this->age = newAge;
+            cout << "Your age has changed." << endl;
+        };
+
+        // para adicionar um método fora da classe,
+        // é necessário que o método esteja declarado.
+        void changeName(string name);
+    private: // acessável apenas dentro do escopo da classe
+        int humanId;
+};
+
+// adicionando método fora da classe
+void Human::changeName(string newName) {
+    this->name = newName;
+    cout << "Your name has changed." << endl;
+}
+
+
+int main() {
+
+    Human people("Elliot", 25);
+    people.printID();
+
+    people.changeName("Mr.Robot");
+    people.changeAge(45);
+    people.printID();
+
+    return 0;
+}
